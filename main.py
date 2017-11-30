ab828db10d22f3105d9cf905b39a6ba1ec915d2b
Grace Lu <gracelu@Graces-MacBook-Air.local>
2017-11-30 13:29:58 -0500

    kinda analyze tokens by pulling out name lines and import statements

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import os, sys, re, tokenize, keyword, io
 
+builtins = [
+    'ArithmeticError','AssertionError','AttributeError','BaseException','BufferError','BytesWarning','DeprecationWarning','EOFError','Ellipsis','EnvironmentError','Exception','False','FloatingPointError','FutureWarning','GeneratorExit','IOError','ImportError','ImportWarning','IndentationError','IndexError','KeyError','KeyboardInterrupt','LookupError','MemoryError','NameError','None','NotImplemented','NotImplementedError','OSError','OverflowError','PendingDeprecationWarning','ReferenceError','RuntimeError','RuntimeWarning',
+    'StandardError','StopIteration','SyntaxError','SyntaxWarning','SystemError','SystemExit','TabError','True','TypeError','UnboundLocalError','UnicodeDecodeError','UnicodeEncodeError','UnicodeError','UnicodeTranslateError','UnicodeWarning','UserWarning','ValueError','Warning','ZeroDivisionError','__IPYTHON__','__IPYTHON__active','__debug__','__doc__','__import__','__name__','__package__','abs','all','any','apply','basestring','bin','bool','buffer','bytearray','bytes','callable','chr','classmethod','cmp','coerce','compile','complex','copyright',
+    'credits','delattr','dict','dir','divmod','dreload','enumerate','eval','execfile','exit','file','filter','float','format','frozenset','getattr','globals','hasattr','hash','help','hex','id','input','int','intern','ip_set_hook','ipalias','ipmagic','ipsystem','isinstance','issubclass','iter','jobs','len','license','list','locals','long',
+    'map','max','min','next','object','oct','open','ord','pow','print','property','quit','range','raw_input','reduce','reload','repr','reversed','round','set','setattr','slice','sorted','staticmethod','str','sum','super','tuple','type','unichr','unicode','vars','xrange','zip'
+]
+
+reserved_words = keyword.kwlist + builtins
+
 def listified_tokenizer(source):
     """Tokenizes *source* and returns the tokens as a list of lists."""
     io_obj = io.StringIO(source)
-    return [list(a) for a in tokenize.generate_tokens(io_obj.readline)]
+    # return [list(a) for a in tokenize.generate_tokens(io_obj.readline)]
+    tokens = []
+    for a in tokenize.generate_tokens(io_obj.readline):
+        tokens.append(list(a))
+    return enumerate_imports(tokens)#pullTypes(tokens)
 
 def enumerate_imports(tokens):
     """
@@ -31,6 +44,14 @@ def enumerate_imports(tokens):
                         if token_string not in imported_modules:
                             imported_modules.append(token_string)
     return imported_modules
+
+def pullTypes(tokens):
+    names = []
+    for next in tokens:
+        if next[0] == 1:
+            if next[4] not in names:
+                names.append(next[4])
+    return names
 
 def p(arr):
 	for next in arr:
